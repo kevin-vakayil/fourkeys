e7829d82e7ff0ac8fc19fac56760836ebd1ac50c
kevin-vakayil <87632641+kevin-vakayil@users.noreply.github.com>
2022-01-12 16:55:39 +0530

    Update incidents.sql

--- queries/incidents.sql
+++ queries/incidents.sql
@@ -3,6 +3,7 @@ source,
 incident_id,
 project_name,
 project_group,
+repo_name,
 MIN(IF(root.time_created < issue.time_created, root.time_created, issue.time_created)) as time_created,
 MAX(time_resolved) as time_resolved,
 ARRAY_AGG(root_cause IGNORE NULLS) changes,
@@ -25,6 +26,7 @@ CASE WHEN source LIKE "github%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.
 CASE WHEN source LIKE "github%" THEN TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.issue.closed_at'))
      WHEN source LIKE "gitlab%" THEN PARSE_TIMESTAMP('%Y-%m-%d %H:%M:%S %z', JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.closed_at')) 
      END AS time_resolved,
+REGEXP_EXTRACT(metadata, r"repo: ([[:alnum:]]*)") as repo_name,
 REGEXP_EXTRACT(metadata, r"root cause: ([[:alnum:]]*)") as root_cause,
 CASE WHEN source LIKE "github%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.issue.labels'), '"name":"Incident"')
      WHEN source LIKE "gitlab%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.object_attributes.labels'), '"title":"Incident"') end as bug,
@@ -32,6 +34,6 @@ FROM four_keys.events_raw
 WHERE event_type LIKE "issue%" OR (event_type = "note" and JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.noteable_type') = 'Issue')
 ) issue
 LEFT JOIN (SELECT time_created, changes FROM four_keys.deployments d, d.changes) root on root.changes = root_cause
-GROUP BY 1,2,3,4
+GROUP BY 1,2,3,4,5
 HAVING max(bug) is True
 ;
